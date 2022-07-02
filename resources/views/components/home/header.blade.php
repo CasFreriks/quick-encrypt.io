0e7dbdfd18b35333d2204a5cec58ae9b0fad3485
Cas Freriks <freriks.cas@gmail.com>
2022-07-02 12:48:37 +0200

    changed the placeholder named 'subject'

--- resources/views/components/home/header.blade.php
+++ resources/views/components/home/header.blade.php
@@ -132,7 +132,7 @@
                                         <div>
                                             <div class="relative border border-gray-300 @error('message_subject') border-red-300 text-red-900 @enderror rounded-md px-3 py-2 shadow-sm focus-within:ring-1 focus-within:ring-indigo-600 focus-within:border-indigo-600">
                                                 <label for="name" class="absolute -top-2 left-2 -mt-px inline-block px-1 bg-white text-xs font-medium text-gray-900">Onderwerp</label>
-                                                <input type="text" name="message_subject" id="name" class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Geheim wachtwoord" value="{{ old('message_subject') }}">
+                                                <input type="text" name="message_subject" id="name" class="block w-full border-0 p-0 text-gray-900 placeholder-gray-500 focus:ring-0 sm:text-sm" placeholder="Vul hier je onderwerp in" value="{{ old('message_subject') }}">
                                             </div>
 
                                             @error('message_subject')
